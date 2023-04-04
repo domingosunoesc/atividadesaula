2fb84e25dc8742ffd05a4b6ec0d688a71ce65d8e
domingosunoesc <111467454+domingosunoesc@users.noreply.github.com>
2023-04-03 22:01:30 -0300

    IF SWITCH

--- FluxoControle/Program.cs
+++ FluxoControle/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string password = "samurai";
+string retorno = string.Empty;
+
+if (password.Length < 10)
+{
+    retorno = @"Sua senha é muito curta.
+              Use ao menos 10 caracteres";
+}
+else if(password.Length >= 15)
+{
+    retorno =  @"Sua senha é muito grande.
+               Use entre 10 e 15 caracteres.";
+}
+else 
+{
+    retorno = @"Sua senha está de acordo.";
+}
+
+Console.WriteLine(retorno);
+
+// Checagem de valor e tipo
+/*
+    EM JavaScript podems fazer assim:
+    var a = "1";
+    let b = 1;
+    (a == b) retornará true
+    (a ===b ) retornará false
+*/
+
+object o = "3";
+int j = 4;
+if( o is int i  )
+{
+    Console.WriteLine($"{i} x {j} = {i * j}");
+}
+else 
+{
+    Console.WriteLine( @"o não é um inteiro,
+                       portnato não é possivel multiplicar");
+}    
+
+// Comando Switch
+int number = Random.Shared.Next(1,7);
+Console.WriteLine($"Meu número aleartório é {number}");
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Um");
+        break;
+    case 2:
+        Console.WriteLine("Dois");
+        break;
+    case 3:
+    case 4:
+        Console.WriteLine("Três ou Quatro");
+        goto case 1;
+    case 5:
+    goto A_label;
+    default:
+        Console.WriteLine("Default");
+        break;
+}
+
+Console.WriteLine("Depois do fim do switch");
+
+A_label:
+Console.WriteLine("Depois de A_label");
+
+
